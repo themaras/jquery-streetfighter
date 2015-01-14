d3d27b7956172dc8dd833eb43fd05d9b8a0b9521
Themistoklis Papaioannou <themaras3@gmail.com>
2015-01-15 01:35:21 +0200

    initial commit

--- js/app.js
+++ js/app.js
@@ -25,6 +25,18 @@ $(document).ready(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
+  $(document).keydown(function(e) {
+  if (e.keyCode == 88){
+    $('.ryu-ready, .ryu-still, .ryu-throwing').hide();
+    $('.ryu-cool').show();
+    }
+  })
+  .keyup(function(e) {
+  if (e.keyCode == 88){
+    $('.ryu-cool, .ryu-ready, .ryu-throwing').hide();
+    $('.ryu-still').show();
+    }
+});
 });
 
 function playHadouken () {
